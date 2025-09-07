80ded77ea82feda18172e5d8b43929f5b84201d9
elloramir <cripboygit@gmail.com>
2025-09-06 21:48:26 -0300

    simplify statusview

--- data/core/statusview.lua
+++ data/core/statusview.lua
@@ -7,19 +7,16 @@ local DocView = require "core.docview"
 local LogView = require "core.logview"
 local View = require "core.view"
 
-
 local StatusView = View:extend()
 
 StatusView.separator  = "  "
 StatusView.separator2 = " | "
-
 
 function StatusView:new()
   StatusView.super.new(self)
   self.message_timeout = 0
   self.message = {}
 end
-
 
 function StatusView:on_mouse_pressed()
   core.set_active_view(core.last_active_view)
@@ -29,7 +26,6 @@ function StatusView:on_mouse_pressed()
   end
 end
 
-
 function StatusView:show_message(icon, icon_color, text)
   self.message = {
     icon_color, style.icon_font, icon,
@@ -38,24 +34,19 @@ function StatusView:show_message(icon, icon_color, text)
   self.message_timeout = system.get_time() + config.message_timeout
 end
 
-
 function StatusView:update()
   self.size.y = style.font:get_height() + style.padding.y * 2
-
   if system.get_time() < self.message_timeout then
     self.scroll.to.y = self.size.y
   else
     self.scroll.to.y = 0
   end
-
   StatusView.super.update(self)
 end
-
 
 local function draw_items(self, items, x, y, draw_fn)
   local font = style.font
   local color = style.text
-
   for _, item in ipairs(items) do
     if type(item) == "userdata" then
       font = item
@@ -65,15 +56,12 @@ local function draw_items(self, items, x, y, draw_fn)
       x = draw_fn(font, color, item, nil, x, y, 0, self.size.y)
     end
   end
-
   return x
 end
 
-
-local function text_width(font, _, text, _, x)
+local function text_width(font, color, text, a, x)
   return x + font:get_width(text)
 end
-
 
 function StatusView:draw_items(items, right_align, yoffset)
   local x, y = self:get_content_offset()
@@ -88,54 +76,28 @@ function StatusView:draw_items(items, right_align, yoffset)
   end
 end
 
-
 function StatusView:get_items()
   if getmetatable(core.active_view) == DocView then
     local dv = core.active_view
-    local line, col = dv.doc:get_selection()
-    local dirty = dv.doc:is_dirty()
-
     return {
-      dirty and style.accent or style.text, style.icon_font, "f",
-      style.dim, style.font, self.separator2, style.text,
-      dv.doc.filename and style.text or style.dim, dv.doc:get_name(),
-      style.text,
-      self.separator,
-      "line: ", line,
-      self.separator,
-      col > config.line_limit and style.accent or style.text, "col: ", col,
-      style.text,
-      self.separator,
-      string.format("%d%%", line / #dv.doc.lines * 100),
+      style.text, dv.doc:get_name(),
     }, {
-      style.icon_font, "g",
-      style.font, style.dim, self.separator2, style.text,
-      #dv.doc.lines, " lines",
-      self.separator,
-      dv.doc.crlf and "CRLF" or "LF"
+      style.text, os.date("%H:%M"),
     }
   end
-
   return {}, {
-    style.icon_font, "g",
-    style.font, style.dim, self.separator2,
-    #core.docs, style.text, " / ",
-    #core.project_files, " files"
+    style.text, os.date("%H:%M"),
   }
 end
 
-
 function StatusView:draw()
   self:draw_background(style.background2)
-
   if self.message then
     self:draw_items(self.message, false, self.size.y)
   end
-
   local left, right = self:get_items()
   self:draw_items(left)
   self:draw_items(right, true)
 end
-
 
 return StatusView
